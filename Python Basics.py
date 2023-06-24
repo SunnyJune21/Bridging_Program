f855f437cf176822d562a7342f6b64b8ed8748ab
Sonia Li <132089675+Sonia0214@users.noreply.github.com>
2023-06-24 18:47:32 -0400

    Update Python Basics.py

--- Python Basics.py
+++ Python Basics.py
@@ -1,31 +1,42 @@
-# 1. Accepting 2 numbers from the user and adding them:
+# 1. Accepting 2 numbers from the user and adding them
 try:
-    a = float(input("Enter the first number "))  # Accept the first number from the user
-    b = float(input("Enter the second number "))  # Accept the second number from the user
-    sum = a + b  # Add the two numbers
-    print("The total is ", sum)  # Print the sum
+    a = float(input("Enter the first number "))
+    b = float(input("Enter the second number "))
+    sum = a + b
+    print("The total is ", sum)
 except ValueError:
     print("Invalid input. Please enter a valid numeric value.")  # Handle the exception if the input is not numeric
 
 
-# 2. Deposit and Sums - Digital Piggy Bank 1.0:
+# 2. Deposit and Sums - Digital Piggy Bank 1.0
 total_sum = 0  # Initialize the total sum to 0
 while True:
     try:
-        income = input("Please enter the amount of money you wish to add or '0' to stop input: ")  # Accept the amount of money from the user
+        income = input("Please enter the amount of money you wish to add or '0' to stop input: ")
         if income == "0":
             break  # Break the loop if the input is '0'
-        income = float(income)  # Convert the input to a floating-point number
+        income = float(income)
         total_sum += income  # Add the income to the total sum
         print(f"The updated total sum is: {total_sum}")  # Print the updated total sum
     except ValueError:
         print("Invalid input. Please enter a valid numeric value.")  # Handle the exception if the input is not numeric
 
 
-# 3. Comparing two user-input numbers and displaying the greater number:
-x = float(input("Enter a number: "))  # Accept the first number from the user
-y = float(input("Enter another number: "))  # Accept the second number from the user
+# 3. Comparing two user-input numbers and displaying the greater number
+x = float(input("Enter a number: "))
+y = float(input("Enter another number: "))
 if x > y:
     print(f"{x} is bigger")  # Print the first number if it is greater
 else:
     print(f"{y} is bigger")  # Print the second number if it is greater
+
+
+# 4. Convert centimeters (cm) to meters (m)
+cm = float(input("Please enter cm: "))
+print(cm, "cm equal to", cm/100, "m")
+
+
+# 5. Convert from Celsius to Fahrenheit
+c = float(input("Enter temperature in Celsius: "))
+f = c*9/5+32
+print(c, "Celsius =",f,"Fahrenheit")
